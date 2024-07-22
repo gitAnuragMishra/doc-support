566119bc0bda855b345a3a2d3de9c6838d2f03db
gitAnuragMishra <heyiamanuragmishra@gmail.com>
2024-07-22 13:45:37 +0530

    chain created

--- runner.py
+++ runner.py
@@ -1,15 +1,26 @@
-#main streamlit runner file
-import os
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceInstructEmbeddings
+#from langchain_community.embeddings import HuggingFaceInstructEmbeddings
 #from langchain_community.embeddings import OpenAIEmbeddings  
-from langchain_community.vectorstores import FAISS
 from sentence_transformers import SentenceTransformer
+#from langchain_community.vectorstores import FAISS
+from langchain_community.vectorstores import Chroma
+import chromadb
+
+
+from langchain.memory import ConversationBufferMemory
+from langchain.chains.retrieval_qa.base import RetrievalQA
+from transformers import pipeline  # type: ignore
+from langchain.retrievers import MultiVectorRetriever
+
+
+#from langchain.chat_models import ChatOpenAI
 
 #os.environ['HF_HOME'] = r'E:\doc-support'
+import os
+os.environ['HF_HOME'] = 'E:\\doc-support'
 
 def get_raw(corpus):
     text=''
@@ -19,28 +30,75 @@ def get_raw(corpus):
             text += pages.extract_text()
     return text
 def get_chunks(raw_text):
-    splitter = CharacterTextSplitter(separator="\n", chunk_size = 1000, chunk_overlap = 200, length_function = len)
+    splitter = CharacterTextSplitter(separator="\n", 
+                                     chunk_size = 1000, 
+                                     chunk_overlap = 200, 
+                                     length_function = len)
     return splitter.split_text(raw_text)
 
+def load_vectordb(embeddings):
+    persistent_client = chromadb.PersistentClient('chroma_db')
+
+    langchain_chroma = Chroma(
+        client=persistent_client,
+        collection_name='pdfs',
+        embedding_function=embeddings,
+    )
+
+    return langchain_chroma
+
 #@st.cache_resource
-def get_embeddings(texts):
+def get_embeddings(chunks):
     #embeddings = OpenAIEmbeddings()
-    model = SentenceTransformer('sentence-transformers/all-MiniLM-L12-v2', cache_folder=r'E:\doc-support')
-    embeddings = model.encode(texts)
-    #vector_store = FAISS.from_texts(texts, embeddings)
-    # embeddings = HuggingFaceInstructEmbeddings(model_name='hkunlp/instructor-xl')
-    #vector_store = FAISS.from_texts(texts, embeddings)
-    #return vector_store
+    model = SentenceTransformer('sentence-transformers/all-MiniLM-L12-v2', 
+                                cache_folder=r'E:\doc-support')
+    #embeddings = HuggingFaceInstructEmbeddings(model_name='hkunlp/instructor-xl')
+    embeddings = model.encode(chunks, 
+                              show_progress_bar = True)
+    #db = FAISS.from_texts(texts=chunks, embedding=embeddings)
+    
+    #------------------#
+    
+    #dimension = embeddings.shape[1] 
     return embeddings
+    
+def get_conversation_chain(vectorstore):
+    llm = pipeline("text-generation", model="distilgpt2")
+
+    def llm_function(prompt):
+        result = llm(prompt, max_length=150, num_return_sequences=1)
+        return result[0]['generated_text']
+    
+    memory = ConversationBufferMemory(
+        memory_key= 'chat_history',
+        return_messages=True
+    )
+
+    retriever = MultiVectorRetriever(vectorstore)
+    conversation_chain = RetrievalQA.from_llm(
+        llm = llm_function(),
+        retriever = retriever,
+        memory=memory
+
+    )
+    return conversation_chain
+
+
+
+
+
 
 def main():
     load_dotenv()
-    st.set_page_config(page_title="DOC SUPPORT", page_icon=':books:')
+    st.set_page_config(page_title="DOC SUPPORT", 
+                       page_icon=':books:')
     st.header("DOC SUPPORT :books:")
     st.text_area("Ask question")
     with st.sidebar:
         st.subheader("Upload .pdf docs")
-        corpus = st.file_uploader('upload here', label_visibility='hidden', accept_multiple_files=True, type='pdf')
+        corpus = st.file_uploader('upload here', 
+                                  label_visibility='hidden', 
+                                  accept_multiple_files=True, type='pdf')
         if st.button("Process"):
             with st.spinner("Extracting contents of pdfs"):
                 raw_text = get_raw(corpus)
@@ -48,13 +106,27 @@ def main():
 
             with st.spinner("Processing text chunks"):
                 text_chunks = get_chunks(raw_text)
-                #st.write(text_chunks)
+                
+                
 
             with st.spinner("Creating vector embeddings"):
-                #vector_store = get_embeddings(text_chunks)
-                #st.write(vector_store.index.ntotal)
-                st.write(get_embeddings(text_chunks))
+                # vector_store = get_embeddings(text_chunks)
+                # st.write(vector_store.index.ntotal)
+                embeddings = get_embeddings(text_chunks)
 
+            with st.spinner("Creating vector db"):
+                # vector_store = get_embeddings(text_chunks)
+                # st.write(vector_store.index.ntotal)
+                vectorstore = load_vectordb(embeddings)
+                st.success("Chroma vectordb successfully created!")
+                
+                # st.write("Chunks created:", len(text_chunks))
+                # st.write("Embedding dimensions:", embeddings.shape[1])
+                st.write(vectorstore)
+
+            with st.spinner("Creating conversation chain"):
+                conversation = get_conversation_chain(vectorstore)
+                st.write(conversation)
     
 
 if __name__ =='__main__':
