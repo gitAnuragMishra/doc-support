d815724f834ad72d49c97b3b90d6280eee6b2151
gitAnuragMishra <heyiamanuragmishra@gmail.com>
2024-08-02 02:39:10 +0530

    llm chat implemented

--- llmchains.py
+++ llmchains.py
@@ -17,31 +17,35 @@ with open('config.yaml', 'r') as f:
     config = yaml.safe_load(f)
 
 
-def create_llm(model_path = config['model']['model_path'], model_type = config['model']['model_type']):
-    llm = CTransformers(model_path, model_type)
+def create_llm(model_path = config['model']['model_path'], model_type = config['model']['model_type'], model_config = config['model']['model_config']):
+    llm = CTransformers(model=model_path, model_type= model_type, config=model_config)
     return llm
 
-def create_llm_chain(embeddings_path = config['embeddings_path']):
+def create_embeddings(embeddings_path = config['embeddings_path']):
 
     embedding_model = HuggingFaceBgeEmbeddings(embeddings_path)
     return embedding_model
 
-def create_chat_memory():
+def create_chat_memory(chat_history):
     return ConversationBufferWindowMemory(memory_key= "history", chat_memory=chat_history, k = 3)
 
 def create_prompt_from_template(template):
     return PromptTemplate.from_template(template)
 
 def create_llm_chain(llm, chat_prompt, memory):
-    return LLMChain(llm, chat_prompt, memory)
+    return LLMChain(llm=llm, prompt= chat_prompt, memory=memory)
 
-def load_normal_chain():
+def load_normal_chain(chat_history):
     return chatChain(chat_history)
 
 class chatChain:
-    def __init__(self, chat_history, model_path = config['model']['model_path'], model_type = config['model']['model_type']):
+    def __init__(self, chat_history):
         self.memory = create_chat_memory(chat_history)
-        llm = create_llm(model_path, model_type)
+        llm = create_llm()
         chat_prompt = create_prompt_from_template(memory_prompt_template)
         self.llm_chain= create_llm_chain(llm, chat_prompt, self.memory)
+
+    def run(self, user_input):
+        return self.llm_chain.run(human_input = user_input, history = self.memory.chat_memory.messages, stop = ['Human:'])
+
 
--- runner.py
+++ runner.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from llmchains import load_normal_chain   
+from langchain.memory import StreamlitChatMessageHistory
 
-
-def load_chain():
-    return load_normal_chain()
+def load_chain(chat_history):
+    return load_normal_chain(chat_history)
 
 def set_send_input():
     st.session_state.send_input = True
@@ -22,6 +22,8 @@ def main():
         st.session_state.send_input = False
         st.session_state.user_question = ' '
 
+    chat_history = StreamlitChatMessageHistory(key='history')
+    llm_chain = load_chain(chat_history)
     chat_container = st.container()
     user_input = st.text_area("ask your question", key = 'user_input' , on_change=set_send_input)
     send_button = st.button("Process", key = 'send_button')
@@ -29,13 +31,14 @@ def main():
     
     if send_button or st.session_state.send_input:
         if st.session_state.user_question != ' ':
-            llm_response = "hello world" 
+            llm_response = llm_chain.run(st.session_state.user_question) 
 
             with chat_container:
                 with st.chat_message('user', avatar= 'human'):
                     st.write(st.session_state.user_question)    #one way of writing code
                     
-                st.chat_message('llm', avatar= 'ai').write('llm_response') #another way
+                st.chat_message('llm', avatar= 'ai').write(llm_response) #another way
+                st.session_state.user_question != ' '
     
 
 
